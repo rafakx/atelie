75bdc45513e2fcab031921913dced794b3a6f3d7
Rafael Rubenich <rafakx@localhost.localdomain>
2016-05-11 00:29:42 -0300

    Criada página de produto, fontes externas

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,5 +1,5 @@
 class Site::HomeController < Site::ApplicationController
   def index
-    @products = Product.order(id: :desc).page(params[:page]).per(12)
+    @products = Product.first(12)
   end
 end
--- app/controllers/site/products_controller.rb
+++ app/controllers/site/products_controller.rb
@@ -1,7 +1,9 @@
 class Site::ProductsController < Site::ApplicationController
   def index
+    @products = Product.order(id: :desc).page(params[:page]).per(16)
   end
 
   def show
+  	@product = Product.find(params[:id])
   end
 end
