3a1656638be8c97ecd28bee1aa30035ad4fc96c0
Rafael Rubenich <rafakx@localhost.localdomain>
2016-06-09 13:28:38 -0300

    Removido faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,61 +7,61 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-administrator = Administrator.create(
-  firstname: "Ilda",
-  lastname: "Bet",
-  username: "ilda",
-  email: "ildabet@hotmail.com",
-  password: "12345"
-)
+# administrator = Administrator.create(
+#   firstname: "Ilda",
+#   lastname: "Bet",
+#   username: "ilda",
+#   email: "ildabet@hotmail.com",
+#   password: "12345"
+# )
 
-16.times do
-  product = Product.create(
-    name: Faker::Lorem.sentence(3),
-    description: Faker::Lorem.paragraph,
-    publish: true,
-    administrator: administrator
-  )
+# 16.times do
+#   product = Product.create(
+#     name: Faker::Lorem.sentence(3),
+#     description: Faker::Lorem.paragraph,
+#     publish: true,
+#     administrator: administrator
+#   )
 
-  category = Category.create(
-    name: Faker::Lorem.word
-  )
+#   category = Category.create(
+#     name: Faker::Lorem.word
+#   )
 
-  product_category = ProductCategory.create(
-    product: product,
-    category: category
-  )
+#   product_category = ProductCategory.create(
+#     product: product,
+#     category: category
+#   )
 
-  detail = Detail.create(
-    name: Faker::Lorem.word,
-    metrics: Faker::Number.number(10)
-  )
+#   detail = Detail.create(
+#     name: Faker::Lorem.word,
+#     metrics: Faker::Number.number(10)
+#   )
 
-  product_detail = ProductDetail.create(
-    product: product,
-    detail: detail
-  )
+#   product_detail = ProductDetail.create(
+#     product: product,
+#     detail: detail
+#   )
 
-  picture = Picture.create(
-    product: product,
-    main:true,
-    image: File.open(Dir.glob(File.join(Rails.root, 'fixtures', 'products', '*')).sample)
-  )
+#   picture = Picture.create(
+#     product: product,
+#     main:true,
+#     image: File.open(Dir.glob(File.join(Rails.root, 'fixtures', 'products', '*')).sample)
+#   )
 
-  picture2 = Picture.create(
-    product: product,
-    main:false,
-    image: File.open(Dir.glob(File.join(Rails.root, 'fixtures', 'products', '*')).sample)
-  )
+#   picture2 = Picture.create(
+#     product: product,
+#     main:false,
+#     image: File.open(Dir.glob(File.join(Rails.root, 'fixtures', 'products', '*')).sample)
+#   )
 
-  visitor = Visitor.create(
-    name: Faker::Name.name,
-    phone: Faker::PhoneNumber.cell_phone,
-    email: Faker::Internet.safe_email
-  )
+#   visitor = Visitor.create(
+#     name: Faker::Name.name,
+#     phone: Faker::PhoneNumber.cell_phone,
+#     email: Faker::Internet.safe_email
+#   )
 
-  message = Message.create(
-    content: Faker::Lorem.paragraph,  
-    visitor: visitor
-  )
-end
+#   message = Message.create(
+#     content: Faker::Lorem.paragraph,  
+#     visitor: visitor
+#   )
+# end
